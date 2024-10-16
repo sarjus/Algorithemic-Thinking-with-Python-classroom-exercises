9b82ab7872651a9414cc5748eb7cebb4d395f54c
Sarju S <sarju.s@sjcetpalai.ac.in>
2024-10-17 05:18:09 +0530

    Create loop_demo1.py

---
+++ loop_demo1.py
@@ -0,0 +1,17 @@
+'''
+Python program to print numbers from 1 to 10 using a while loop.
+
+Author: Sarju S
+'''
+
+# Initialize the counter
+num = 1
+
+# Using a while loop to print numbers from 1 to 10
+while num <= 10:
+    print(num)
+    num += 1  # Increment the counter
+
+
+
+
