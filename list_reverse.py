131c965189d2e1ee00a82c85273020aa8d84cedc
Sarju S <sarju.s@sjcetpalai.ac.in>
2024-10-24 07:01:44 +0530

    Create list_reverse.py

---
+++ list_reverse.py
@@ -0,0 +1,19 @@
+'''
+Objective: Use the reverse() method to reverse the order of elements in a list.
+Instructions:
+Create a list of numbers: [10, 20, 30, 40, 50].
+Reverse the order of the list.
+Print the reversed list.
+
+
+Author: Sarju S
+Date: 24-10-2024
+'''
+# Step 1: Creating the list of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Step 2: Reversing the list
+numbers.reverse()
+
+# Step 3: Printing the reversed list
+print(numbers)  # Output: [50, 40, 30, 20, 10]
