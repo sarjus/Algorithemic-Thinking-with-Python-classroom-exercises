bd8f98f5c56d33325360c2be84de225f75bee13b
Sarju S <sarju.s@sjcetpalai.ac.in>
2024-11-13 06:36:43 +0530

    Create set-discount-codes.py

---
+++ set-discount-codes.py
@@ -0,0 +1,20 @@
+'''
+Description:
+       A store has a set of discount codes, but all codes have expired,
+       so they need to be removed. Write a program to clear the discount codes.
+
+
+Author: Sarju S
+Date: November 13, 2024
+Version: 1.0
+'''
+# Set of discount codes
+discount_codes = {"DISC10", "SALE20", "SAVE15"}
+
+# Clear all discount codes
+discount_codes.clear()
+
+# Output to confirm the set is empty
+print("Discount codes after clearing:", discount_codes)
+
+
